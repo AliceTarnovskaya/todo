98e0653cf790b7dffd4d2115068b2bd170d3649d
AliceTarnovskaya <alice.tarnovskaya@gmail.com>
2021-09-09 22:14:47 +0300

    second

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     let newTask = document.createElement('p');
     newTask.classList.add('itemText');
-    newTask.innerHTML = `<input type="checkbox" id="done">${input.value} <span class="delete"></span> <span class="edit"></span>`;
+    newTask.innerHTML = `<input type="checkbox" id="done"><span class="text">${input.value}</span> <span class="delete"></span> <span class="edit"></span>`;
     result.prepend(newTask);
     input.value = '';
-
-
 
     const checkbox = newTask.querySelector("#done");
 
@@ -34,22 +32,21 @@ form.addEventListener('submit', (e) => {
 
     const editElement = newTask.querySelector('.edit');
     editElement.addEventListener('click', () => {
-        const newForm = document.createElement('form');
-        const newInput = document.createElement('input');
-        const newButton = document.createElement('button');
+        const newForm = document.createElement('form'),
+            newInput = document.createElement('input'),
+            newButton = document.createElement('button'),
+            text = newTask.querySelector('.text');
         newButton.innerText = 'Save';
         newForm.appendChild(newInput);
         newForm.appendChild(newButton);
         newTask.append(newForm);
 
+
         newForm.addEventListener('submit', (e) => {
             e.preventDefault();
-            newTask.innerHTML = `<input type="checkbox" id="done">${newInput.value} <span class="delete"></span> <span class="edit"></span>`;
-        })
-
-
-    })
-
-
-
+            text.innerText = newInput.value;
+            newForm.reset();
+            newTask.removeChild(newForm);
+        });
+    });
 });

1fcc6478c0ef8dea43e9e3b09c13c59475334e6e
AliceTarnovskaya <alice.tarnovskaya@gmail.com>
2021-09-16 21:23:33 +0300

    created functions, work with LS

--- script.js
+++ script.js
@@ -9,66 +9,76 @@ if (localStorage.tasks) {
     tasks = JSON.parse(localStorage.tasks);
 }
 
-for (let task of tasks) {
+function createTaskNode(taskText) {
     let newTask = document.createElement('p');
     newTask.classList.add('result__item');
-    newTask.innerText = task;
-    result.prepend(newTask);
-}
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let newTask = document.createElement('p');
-
-    tasks.push(input.value);
-    localStorage.tasks = JSON.stringify(tasks);
-
-    newTask.classList.add('result__item');
-    // task = input.value;
-    newTask.innerHTML = `<input type="checkbox" id="done"><span class="text">${input.value}</span> <span class="edit"></span><span class="delete"></span>`;
+    newTask.innerHTML = `<input type="checkbox" id="done"><span class="text">${taskText}</span> <span class="edit"></span><span class="delete"></span>`;
     result.prepend(newTask);
     input.value = '';
 
-    const checkbox = newTask.querySelector("#done");
+    checkedAsDone(newTask);
+    deleteTask(newTask);
+    editTask(newTask);
+}
+
+function checkedAsDone(createdTask) {
+    const checkbox = createdTask.querySelector("#done");
 
     checkbox.addEventListener("click", () => {
         if (checkbox.checked) {
             checkbox.parentElement.style.textDecoration = "line-through";
             checkbox.parentElement.style.color = "#4f555e";
 
-            result.appendChild(newTask);
+            result.appendChild(createdTask);
         } else {
             checkbox.parentElement.style.textDecoration = "none";
             checkbox.parentElement.style.color = "#e0e0e0";
-            result.prepend(newTask)
+            result.prepend(createdTask)
         }
     });
+}
 
-    const deletedElement = newTask.querySelector('.delete');
+function deleteTask(createdTask) {
+    const deletedElement = createdTask.querySelector('.delete');
 
     deletedElement.addEventListener('click', () => {
         deletedElement.parentElement.remove();
     });
 
-    const editElement = newTask.querySelector('.edit');
+}
+
+function editTask(createdTask) {
+    const editElement = createdTask.querySelector('.edit');
     editElement.addEventListener('click', () => {
         const newForm = document.createElement('form'),
             newInput = document.createElement('input'),
             newButton = document.createElement('button'),
-            text = newTask.querySelector('.text');
+            text = createdTask.querySelector('.text');
         newForm.classList.add('subform');
         newInput.classList.add('input__edit');
         newButton.innerText = 'Save';
         newForm.appendChild(newInput);
         newForm.appendChild(newButton);
-        newTask.append(newForm);
-
-
+        createdTask.append(newForm);
         newForm.addEventListener('submit', (e) => {
             e.preventDefault();
             text.innerText = newInput.value;
+            tasks.push(newInput.value);
+            localStorage.tasks = JSON.stringify(tasks);
+            // localStorage.removeItem(newForm);
             newForm.reset();
-            newTask.removeChild(newForm);
+            createdTask.removeChild(newForm);
         });
     });
+}
+
+for (let task of tasks) {
+    createTaskNode(task);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    tasks.push(input.value);
+    localStorage.tasks = JSON.stringify(tasks);
+    createTaskNode(input.value);
 });
